abbd8e7055308948a891a28b2b98a3249524474f
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-04-09 14:51:16 +0200

    Solve day 3 part 2

--- src/day03/index.js
+++ src/day03/index.js
@@ -9,57 +9,48 @@ const dirs = {
   R: [1, 0],
 }
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
+const intersections = (input) => {
+  const paths = parseInput(input)
   const seen = {}
-  const crash = {}
-  let min = Number.MAX_VALUE
-  input.forEach((path, j) => {
+  const intersections = {}
+  paths.forEach((path, j) => {
     let x = 0
     let y = 0
+    var totalSteps = 0
     path.forEach(([dir, steps]) => {
       const [dx, dy] = dirs[dir]
       for (let i = 1; i <= steps; i++) {
+        totalSteps++
         x += dx
         y += dy
         const key = 100000*x + y
-        if (seen[key] == j) {
-          min = Math.min(Math.abs(x) + Math.abs(y), min)
-          crash[[x, y]] = true
+        if (seen[key]?.[0] == j) {
+          intersections[[x, y]] = seen[key][1]+totalSteps
         }
-        seen[key] = j+1
+        seen[key] = [j+1, totalSteps]
       }
     })
   })
-  console.log(crash);
-  return min
+  return intersections
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
-
-  return
+const part1 = (input) => {
+  const manhattan = p => {
+    const [x, y] = p.split(',')
+    return Math.abs(x) + Math.abs(y)
+  }
+  return Math.min(...Object.keys(intersections(input)).map(manhattan))
 }
 
-const part1Input = `R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`
-const part2Input = part1Input
+const part2 = (input) => {
+  return Math.min(...Object.values(intersections(input)))
+}
+
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: 159 },
-      { input: `R8,U5,L5,D3
-U7,R6,D4,L4`, expected: 6 },
-      { input: `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`, expected: 135 },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
