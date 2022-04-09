ad6e1fa4a878cdf517c42a1cbd59fc241af42b3e
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-04-09 13:37:14 +0200

    Solve day 1 part 2

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.121ms
-Time part 2: -
-Both parts: 0.121279ms
+Time part 1: 0.058ms
+Time part 2: 0.751ms
+Both parts: 0.809033ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 0.121ms
+Total stars: 2/50
+Total time: 0.809ms
 ```
 
 <!--/RESULTS-->
--- src/day01/index.js
+++ src/day01/index.js
@@ -1,30 +1,15 @@
 import run from "aocrunner"
 
-const parseInput = rawInput => rawInput.split('\n').map(x => +x)
+const solve = (input, f) => input.split('\n').reduce((acc, x) => acc + f(x), 0)
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-  const f = x => Math.floor(x / 3) - 2
-  return input.reduce((acc, x) => acc + f(x), 0)
-}
+const fuel = x => Math.floor(x / 3) - 2
+const fuelRec = x => fuel(x) > 0 ? fuel(x) + fuelRec(fuel(x)) : 0
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
-
-  return
-}
-
-const part1Input = ``
-const part2Input = part1Input
 run({
   part1: {
-    solution: part1,
+    solution: input => solve(input, fuel),
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
-    solution: part2,
+    solution: input => solve(input, fuelRec),
   },
-  onlyTests: false,
 })
