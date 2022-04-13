c2fb320221b26198e32d657a5e0661f015f2e4c5
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-04-13 20:01:54 +0200

    Refactor day 23

--- src/day23/index.js
+++ src/day23/index.js
@@ -1,57 +1,53 @@
 import run from "aocrunner"
 import Intcode from "../utils/intcode.js"
 
-const part1 = (program) => {
+const transmit = (computers, setNAT) => {
+  var transmitted = false
+  computers.forEach(computer => {
+    const output = computer.output()
+    for (var i = 0; i < output.length-1; i+=3) {
+      const [addr, x, y] = output.slice(i)
+      if (addr == 255) {
+        setNAT([x, y])
+      } else {
+        transmitted = true
+        computers[addr].input([x,y])
+      }
+    }
+  })
+  return transmitted
+}
+
+const network = (program, setNAT) => {
   const computers = [...Array(50)].map((_,i) => new Intcode(program).input([i,-1]))
 
-  var ans = 0
-  for (var j = 0; !ans; j++) {
-    computers.forEach(computer => {
-      const output = computer.output()
-      for (var i = 0; i < output.length-1; i+=3) {
-        const [addr, x, y] = output.slice(i)
-        if (addr == 255) {
-          ans = y
-        } else {
-          computers[addr].input([x,y])
-        }
-      }
-    })
+  return [() => transmit(computers, setNAT), computers[0]]
+}
+
+const part1 = (program) => {
+  let ans
+  const [transmit] = network(program, ([_, y]) => ans = y)
+
+  while(!ans) {
+    transmit()
   }
   return ans
 }
 
 const part2 = (program) => {
-  const computers = [...Array(50)].map((_,i) => new Intcode(program).input([i,-1]))
-
-  var ans = 0
-
-  var last = -Infinity
-  var nat = []
-  for (var j = 0; !ans; j++) {
-    var transmitted = false
-    computers.forEach(computer => {
-      const output = computer.output()
-      for (var i = 0; i < output.length-1; i+=3) {
-        const [addr, x, y] = output.slice(i)
-        if (addr == 255) {
-          nat = [x, y]
-        } else {
-          transmitted = true
-          computers[addr].input([x,y])
-        }
-      }
-    })
-    if (!transmitted) {
+  let ans, last
+  let nat = []
+  const [transmit, com0] = network(program, (arr) => nat = arr)
+  while(!ans) {
+    if (!transmit()) {
       const y = nat[1]
       if (last == y) {
         return y
       }
       last = y
-      computers[0].input(nat)
+      com0.input(nat)
     }
   }
-  return ans
 }
 
 run({
